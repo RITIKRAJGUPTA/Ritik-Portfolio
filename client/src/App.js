f894fe1f93325be8da495ea8ccafa48f764a5405
ऋतिक राज गुप्ता <142139641+RITIKRAJGUPTA@users.noreply.github.com>
2025-04-10 23:40:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/send", {
+      const res = await fetch("https://ritik-portfolio.onrender.com/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
